1967eec52851d854d2d5561393c87c06050c85fd
chitrashrivastava <chitrashrivastava64@gmail.com>
2024-02-29 17:30:59 +0530

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css'; // Importing CSS module with styles object
 import Heading from './Head';
 
 const App = () => {
+  const [username, setUsername] = useState("Chitra");
+  
+  const handleChange = (e) => {
+    setUsername(e.target.value); // Update the username state with the input value
+    console.log(e.target.value); 
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+    console.log(username)
+  };
+
+
   return (
     <>
-      <h1 className={styles.heading}>Hi there</h1> {/* Applying CSS module class */}
+      <h1 className={styles.heading}>Hi there</h1> 
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
       <Heading />      
     </>
   );

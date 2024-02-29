c6e609c6c9b4e318290dc5b1a35d737c170150d3
chitrashrivastava <chitrashrivastava64@gmail.com>
2024-02-29 17:43:48 +0530

    practice usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.css'; // Importing CSS module with styles object
 import Heading from './Head';
 
 const App = () => {
   const [username, setUsername] = useState("Chitra");
+  const [password, setPassword] = useState("1234");
   
-  const handleChange = (e) => {
+  
+  const handleChangeusername = (e) => {
     setUsername(e.target.value); // Update the username state with the input value
     console.log(e.target.value); 
   };
-
+const handleChangepassword=(e)=>{
+  setPassword(e.target.value)
+  console.log(e.target.value)
+}
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(e)
-    console.log(username)
+    console.log(username),
+    console.log(password)
   };
 
 
@@ -21,10 +27,12 @@ const App = () => {
     <>
       <h1 className={styles.heading}>Hi there</h1> 
       <form onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={handleChange} />
+        <input type="text" value={username} onChange={handleChangeusername} />
+        <input type="text" value={password} onChange={handleChangepassword} />
+
         <button type="submit">Submit</button>
       </form>
-      <Heading />      
+        
     </>
   );
 };
